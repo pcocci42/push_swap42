502d9b393b431319a71aa3e9a1db7745c8439387
pcocci42 <paolo.cocci@arts.cornish.edu>
2023-02-24 11:04:10 +0100

    gg

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcocci <pcocci@student.42firenze.it>       +#+  +:+       +#+        */
+/*   By: pcocci <pcocci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 10:40:46 by pcocci            #+#    #+#             */
-/*   Updated: 2023/02/24 10:31:04 by pcocci           ###   ########.fr       */
+/*   Updated: 2023/02/24 11:03:02 by pcocci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,43 +33,44 @@ typedef struct s_operations
 	int	rrr;
 }	t_operations;
 
-void	swap_a(int *stack, t_size *size);
-void	sa(int *stack, t_size *size);
-void	swap_b(int *stack, t_size *size);
-void	sb(int *stack, t_size *size);
-void	swap_all(int *stack_a, int *stack_b, t_size *size);
-void	push_b(int *stack_a, int *stack_b, t_size *size);
-void	push_a(int *stack_a, int *stack_b, t_size *size);
-void	pa(int *stack_a, int *stack_b, t_size *size);
-void	pb(int *stack_a, int *stack_b, t_size *size);
-void	rotate_a(int *stack, t_size *size);
-void	ra(int *stack, t_size *size);
-void	rotate_b(int *stack, t_size *size);
-void	rb(int *stack, t_size *size);
-void	rotate_all(int *stack_a, int *stack_b, t_size *size);
-void	reverse_ra(int *stack, t_size *size);
-void	rra(int *stack, t_size *size);
-void	reverse_rb(int *stack, t_size *size);
-void	rrb(int *stack, t_size *size);
-void	rrr(int *stack_a, int *stack_b, t_size *size);
-void	swap(int *a, int *b);
-void	sort_3(int *stack_a, t_size *size);
-void	cases(int *stack_a, int top, int mid, t_size *size);
-void	correct_a(int *stack_a, t_size *size);
-void	print_stacks(int *stack_a, int *stack_b, t_size *size);
-void	count_r(int index, int size, t_operations *op, int a);
-void	fuck_bzero(void *s, int n);
-void	sort_a(int *stack_a, int *stack_b, t_size *size);
-void	sort_b(int *stack_a, int *stack_b, t_size *size);
-int		min(int *stack, int size);
-void	less_op(t_operations *op);
-int		diff(t_operations min, t_operations op);
-void	parse(char *str);
-int		ft_atoi2(const char *str);
-int		count_words(const char *str, char c);
-void	sort(int *stack_a, int n);
-size_t	ft_strlen2(const char *str);
-int		check_many(char **av);
-int		check_many2(char **av);
+void		swap_a(int *stack, t_size *size);
+void		sa(int *stack, t_size *size);
+void		swap_b(int *stack, t_size *size);
+void		sb(int *stack, t_size *size);
+void		swap_all(int *stack_a, int *stack_b, t_size *size);
+void		push_b(int *stack_a, int *stack_b, t_size *size);
+void		push_a(int *stack_a, int *stack_b, t_size *size);
+void		pa(int *stack_a, int *stack_b, t_size *size);
+void		pb(int *stack_a, int *stack_b, t_size *size);
+void		rotate_a(int *stack, t_size *size);
+void		ra(int *stack, t_size *size);
+void		rotate_b(int *stack, t_size *size);
+void		rb(int *stack, t_size *size);
+void		rotate_all(int *stack_a, int *stack_b, t_size *size);
+void		reverse_ra(int *stack, t_size *size);
+void		rra(int *stack, t_size *size);
+void		reverse_rb(int *stack, t_size *size);
+void		rrb(int *stack, t_size *size);
+void		rrr(int *stack_a, int *stack_b, t_size *size);
+void		swap(int *a, int *b);
+void		sort_3(int *stack_a, t_size *size);
+void		cases(int *stack_a, int top, int mid, t_size *size);
+void		correct_a(int *stack_a, t_size *size);
+void		print_stacks(int *stack_a, int *stack_b, t_size *size);
+void		count_r(int index, int size, t_operations *op, int a);
+void		fuck_bzero(void *s, int n);
+void		sort_a(int *stack_a, int *stack_b, t_size *size);
+void		sort_b(int *stack_a, int *stack_b, t_size *size);
+int			min(int *stack, int size);
+void		less_op(t_operations *op);
+int			diff(t_operations min, t_operations op);
+void		parse(char *str);
+int			count_words(const char *str, char c);
+void		sort(int *stack_a, int n);
+size_t		ft_strlen2(const char *str);
+int			check_many(char **av);
+int			check_many2(char **av);
+int			check_int(char *str);
+long int	ft_atoi2(const char *str);
 
 #endif
--- srcs/atoi.c
+++ srcs/atoi.c
@@ -3,40 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   atoi.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcocci <pcocci@student.42firenze.it>       +#+  +:+       +#+        */
+/*   By: pcocci <pcocci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 11:10:27 by pcocci            #+#    #+#             */
-/*   Updated: 2023/02/24 10:32:03 by pcocci           ###   ########.fr       */
+/*   Updated: 2023/02/24 11:01:45 by pcocci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 #include "../libft/includes/libft.h"
 
-int	whitespaces(const char *str)
+long int	ft_atoi2(const char *str)
 {
+	long int	sign;
+	long int	result;
 	int	i;
 
 	i = 0;
-	if (str[i] == 43 && str[i + 1] != 45)
-		i++;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32 || str[i] == '0')
-		i++;
-	return (i);
-}
-
-int	ft_atoi2(const char *str)
-{
-	int	sign;
-	int	result;
-	int	i;
-
 	sign = 1;
 	result = 0;
-	i = whitespaces(str);
 	if (str[i] == '-')
 	{
-		sign *= -1;
+		sign = -1;
 		i++;
 	}
 	if (sign != 1 && ft_strlen2(str) > 11)
